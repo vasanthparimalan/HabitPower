af46645e4bc9dafeaffa670776e8cdf3d88f8cc5
vasanthparimalan <parimalanvasanth.ee@gmail.com>
2026-04-13 00:58:17 +0530

    Theme BugFix

--- app/src/androidTest/java/com/example/habitpower/data/HabitPowerDatabaseMigrationTest.kt
+++ app/src/androidTest/java/com/example/habitpower/data/HabitPowerDatabaseMigrationTest.kt
@@ -4,11 +4,11 @@ import androidx.room.testing.MigrationTestHelper
 import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
 
 @RunWith(AndroidJUnit4::class)
 class HabitPowerDatabaseMigrationTest {
